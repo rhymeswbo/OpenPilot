0c8c77844f8b630768077a0249c71d043941d005
Giovanni Rey <rhymeswbo@gmail.com>
2015-02-16 11:51:36 -0800

    work in progress, new failsafe code, currently not compliing

---
+++ flight/modules/failsafeManager/failsafeModes.cpp
@@ -0,0 +1,9 @@
+//
+//  failsafeModes.cpp
+//  OpenPilotOSX
+//
+//  Created by GIOVANNI REY on 2/10/15.
+//  Copyright (c) 2015 OpenPilot. All rights reserved.
+//
+
+#include "failsafeModes.h"
---
+++ flight/modules/failsafeManager/failsafeModes.h
@@ -0,0 +1,14 @@
+//
+//  failsafeModes.h
+//  OpenPilotOSX
+//
+//  Created by GIOVANNI REY on 2/10/15.
+//  Copyright (c) 2015 OpenPilot. All rights reserved.
+//
+
+#ifndef __OpenPilotOSX__failsafeModes__
+#define __OpenPilotOSX__failsafeModes__
+
+#include <stdio.h>
+
+#endif /* defined(__OpenPilotOSX__failsafeModes__) */
